1e3d8ec50ce774b2fbd503ab4a8d8f11eeae4349
jihenebulteau297-create <jihenebulteau297@gmail.com>
2026-03-30 21:22:36 +0800

    第二周作业一

--- AT24C02/main.c
+++ AT24C02/main.c
@@ -11,27 +11,27 @@ unsigned char keynum;
 
 void UART_Routine() interrupt 4
 {
-	if(RI==1){
-		UART_SendByte(SBUF);
-		num0=SBUF;
-		AT24C02_WriteByte(0,num0);
-		Delay(5);
-		RI=0;
+	if(RI==1){							//判断是负有数据传进来
+		UART_SendByte(SBUF);				
+		num0=SBUF;						//中间值
+		AT24C02_WriteByte(0,num0);		//往AT24C02的地址0写传进来的数据
+		Delay(5);						//等数据完全写好
+		RI=0;							//允许下次串口发数据进来
 	}
 }
 
 
 void main(){
 	
-	UART_Init();
+	UART_Init();						//初始化串口
 	
 	
 	while(1){
-		keynum=Key();
+		keynum=Key();					//扫描按键
 		
-		if(keynum==3){
-			baocun=AT24C02_ReadByte(0);
-			UART_SendByte(baocun);
+		if(keynum==3){					//如果按了按键3
+			baocun=AT24C02_ReadByte(0);	//读取AT24C02的地址0里的数据
+			UART_SendByte(baocun);		//往电脑发送上次传进来的数据
 		}
 	}
 }
